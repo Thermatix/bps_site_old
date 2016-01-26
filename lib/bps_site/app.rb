bc196843b3058fcd636fbe88e9554dd2d2fd07bc
Martin Becker <mbeckerwork@gmail.com>
2016-01-26 18:09:31 +0000

    fixes incorrect application root, adds helpers, adds index root

--- lib/bps_site/app.rb
+++ lib/bps_site/app.rb
@@ -2,15 +2,21 @@ module BpsSite
   class App < Sinatra::Base
 
     configure do
-      set :root, File.expand_path('../..', __FILE__)
+      set :root,File.expand_path('../../../', __FILE__)
       set :opal_libs, %w{opal ruta reactive-ruby opal-browser}
     end
-
 
     include Asset_Pipeline
 
     use Landing
     use Members
     use Admin
+
+    helpers Helpers
+
+    get "/" do
+      erb :index
+    end
+
   end
 end
