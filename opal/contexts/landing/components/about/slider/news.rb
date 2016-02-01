176d3d1e16864e2a0b96e20444bf8af74539f037
Martin Becker <mbeckerwork@gmail.com>
2016-02-01 23:05:08 +0000

    attempts to add working news segment to slider

--- opal/contexts/landing/components/about/slider/news.rb
+++ opal/contexts/landing/components/about/slider/news.rb
@@ -1,8 +1,38 @@
 module Components
   module Landing
     class Slider::News < Base
+
+      define_state listing: {}
+
+      before_mount do
+        div style: scrollable do
+          ul class_name: m_col(12) ,style: no_bullets do
+            state.listing.each do |id,item|
+              li id: id do
+                h2 class_name: m_div("bottom") do
+                  item[:date] + ' - ' + item[:title]
+                end
+
+                p style: scrollable.merge({height: "200px"}) do
+                  item[:body]
+                end
+              end
+            end
+          end
+        end
+      end
+
       def render
         "news"
+      end
+
+
+      def get_listing
+        Request.get('/api/news/public') do |request|
+          handle_response_for request do |response|
+            state.listing! response.json
+          end
+        end
       end
     end
   end
